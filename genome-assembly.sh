ef7ee5accc4e0bcc6b5051ba992d2be764b5ec9a
Leinonen Miika P M <xlex@lx8-cs-02.pc.helsinki.fi>
2019-05-30 22:46:34 +0300

    All pieces included

---
+++ genome-assembly.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Target genome
+TARGET="ecoli"
+#TARGET="scerevisiae"
+#TARGET="celegans"
+#TARGET="arabidopsis"
+
+# Provided data
+REFERENCE="data/"$TARGET"-reference.fasta"
+ERR_READS="data/"$TARGET"-reads.fasta"
+
+# Correcting
+COR_READS="data/"$TARGET"-reads-corrected.fasta"
+
+# Other stuff
+
+READ_READ_ALIGN_ZIP="data/"$TARGET"-reads-reads-alignment.paf.gz"
+MINIASM_GFA="data/"$TARGET"-miniasm-output.gfa"
+PRECOLOR_CONTIGS="data/"$TARGET"-precolor-contigs.fasta"
+
+PRECOLOR_CONTIGS_OPT_MAP="data/"$TARGET"-precolor-contigs.valouev"
+REFERENCE_OPT_MAP="data/"$TARGET"-reference.valouev"
+
+CONTIG_REFERENCE_MAPPING="data/"$TARGET"-contig-reference-mapping-output.txt"
+
+CONTIGS_READS_ALIGN="data/"$TARGET"-contigs-reads-alignment.paf"
+
+COLORED_CONTIGS="data/"$TARGET"-precolor-contig-colors.cval"
+COLORED_READS="data/"$TARGET"-read-colors.cf"
+ADJ_COLORED_READS="data/"$TARGET"-read-colors-adjusted.cf"
+
+KERMIT_GFA="data/"$TARGET"-kermit-output.gfa"
+KERMIT_GFA_NP="data/"$TARGET"-kermit-np-output.gfa"
+
+POSTCOLOR_CONTIGS="data/"$TARGET"-postcolor-contigs.fasta"
+POSTCOLOR_CONTIGS_NP="data/"$TARGET"-postcolor-contigs-np.fasta"
+
+
+
+###
+### 1. Correct reads with CONSENT
+###
+
+#CONSENT/CONSENT-correct --in $ERR_READS --out $COR_READS --type PB
+
+###
+### 2. Run minimap with corrected reads 
+###
+
+minimap2/minimap2 -x ava-pb -t8 $COR_READS $COR_READS | gzip -1 > $READ_READ_ALIGN_ZIP
+#miniasm/minimap2/minimap2 -x ava-pb -t8 $ERR_READS_CORRECTED_REORDERED $ERR_READS_CORRECTED_REORDERED > $READ_READ_PAFZIP
+
+
+###
+### 3. Run miniasm to get contigs 
+###
+
+miniasm/miniasm -f $COR_READS $READ_READ_ALIGN_ZIP > $MINIASM_GFA
+awk '$1 ~/S/ {print ">"$2"\n"$3}' $MINIASM_GFA | fold > $PRECOLOR_CONTIGS
+
+
+###
+### 4. Create optical maps of contigs
+###
+
+#python3 optical_map_generator.py $PRECOLOR_CONTIGS -o $PRECOLOR_CONTIGS_OPT_MAP
+
+
+### 
+### 5. Create optical map of reference
+###
+
+#python3 optical_map_generator.py $REFERENCE -o $REFERENCE_OPT_MAP
+
+
+###
+### 6. Run valouev to map contigs to reference 
+###
+
+#valouev_optmap_alignment/fit/fit $REFERENCE_OPT_MAP $PRECOLOR_CONTIGS_OPT_MAP | tee $CONTIG_REFERENCE_MAPPING
+
+
+###
+### 7. Run minimap with corrected reads and contigs 
+###
+
+#minimap2/minimap2 -t8 -x ava-pb $PRECOLOR_CONTIGS $COR_READS > $READ_CONTIG_ALIGN
+
+
+###
+### 8.1 Color contigs
+###
+
+#python3 contig_colorer.py $CONTIG_REFERENCE_MAPPING $REFERENCE_OPT_MAP $PRECOLOR_CONTIGS_OPT_MAP -c $COLORED_CONTIGS
+
+###
+### 8.2 Color reads based on contig colors and alignments
+###
+
+#python3 read_colorer.py $READ_CONTIG_ALIGN $COLORED_CONTIGS -r $COLORED_READS -e -s 
+
+###
+### 9. Adjust colorings
+###
+
+#python3 read_recolorer.py $COLORED_READS $ADJ_COLORED_READS
+
+###
+### 10. Run kermit
+###
+
+#miniasm/minimap2/minimap2 -t8 -x ava-pb $ERR_READS_CORRECTED_REORDERED $ERR_READS_CORRECTED_REORDERED > $READ_READ_PAF
+
+
+#kermit/kermit -C $ADJ_COLORED_READS -f $COR_READS $READ_READ_ALIGN_ZIP > $KERMIT_GFA
+#kermit/kermit -C $ADJ_COLORED_READS -f $COR_READS $READ_READ_ALIGN_ZIP -P > $KERMIT_GFA_NP
+
+#awk '$1 ~/S/ {print ">"$2"\n"$3}' $KERMIT_GFA > $POSTCOLOR_CONTIGS
+#awk '$1 ~/S/ {print ">"$2"\n"$3}' $KERMIT_GFA_NP > $POSTCOLOR_CONTIGS_NP
+
+#python3 quast/quast-5.0.2/quast.py -r $REFERENCE $POSTCOLOR_CONTIGS
+#python3 quast/quast-5.0.2/quast.py -r $REFERENCE $POSTCOLOR_CONTIGS_NP
+
+
+
+
---
+++ read_recolorer.py
@@ -0,0 +1,78 @@
+import argparse
+import os
+from file_opener import open_cf
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(dest='read_colorings', action='store',
+                        help='Path to the read colors')
+    parser.add_argument('-a', '--adjusted_colors_path', dest='adj_colors', action='store', default="use_default",
+                        help='Path where to save adjusted read colors')
+    results = parser.parse_args()
+    return results
+
+
+def find_index(color, clist):
+    for i in range(len(clist)):
+        if clist[i] == color:
+            return i
+    return 0
+
+
+def main(args):
+
+    print("== Initializing ==")
+    coloring_path = args.read_colorings
+    new_coloring_path = args.adj_colors
+    
+    if new_coloring_path == "use_default":
+        if not os.path.exists("./data"):
+            os.makedirs("./data")
+        new_coloring_path = "./data/colored_reads_adjusted.cf"
+
+    colorings = open_cf(coloring_path)
+    multiplier = int("1" + (len(str(colorings[0][1])) - 1) * "0")
+    big_shift = len(str(multiplier)) - 1
+
+    colors = {}
+    #oo = 0
+    print("-- Done --")
+
+    print("== Finding used colors ==")
+    for cr in colorings:
+        #oo += 1
+        chromos = str(cr[1])[:-big_shift]
+        #print(oo, cr, chromos)
+        if colors.get(chromos, "na") == "na":
+            colors[chromos] = []
+        first_color = cr[1] - int(chromos) * multiplier
+        second_color = cr[2] - int(chromos) * multiplier
+        if first_color not in colors[chromos]:
+            colors[chromos].append(first_color)
+        if second_color not in colors[chromos]:
+            colors[chromos].append(second_color)
+
+    for key in colors.keys():
+        colors[key].sort()
+        print("-- Done --")
+
+    print("== Writing adjusted coloring ==")
+    with open(new_coloring_path, 'w') as wfile:
+        for cr in colorings:
+            chromos = str(cr[1])[:-big_shift]
+            first_color = cr[1] - int(chromos) * multiplier
+            second_color = cr[2] - int(chromos) * multiplier
+
+            si = find_index(first_color, colors[chromos])
+            ei = find_index(second_color, colors[chromos])
+
+            wfile.write(cr[0] + "\t" + str(si + int(chromos) * multiplier) + "\t" + str(ei + int(chromos) * multiplier) + "\n")
+
+    print("-- Done --")
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
+
